6a165b71a881805c9ed9e1c45667517baa8013aa
Prospector <thedoctorsoda@gmail.com>
2017-12-18 21:01:55 -0800

    Fix model depth

--- src/java/mechconstruct/gui/blueprint/elements/SlotConfigPopupElement.java
+++ src/java/mechconstruct/gui/blueprint/elements/SlotConfigPopupElement.java
@@ -1,12 +1,17 @@
 package mechconstruct.gui.blueprint.elements;
 
+import mechconstruct.block.BlockMachine;
 import mechconstruct.blockentities.BlockEntityMachine;
 import mechconstruct.gui.MechGui;
 import mechconstruct.gui.blueprint.Sprite;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.client.FMLClientHandler;
 
 public class SlotConfigPopupElement extends ElementBase {
@@ -21,28 +26,32 @@ public class SlotConfigPopupElement extends ElementBase {
 	public void draw(MechGui gui) {
 		super.draw(gui);
 		BlockEntityMachine machine = ((BlockEntityMachine) gui.provider);
+		IBlockAccess blockAccess = machine.getWorld();
+		BlockPos pos = machine.getPos();
+		IBlockState state = blockAccess.getBlockState(pos);
 		BlockRendererDispatcher dispatcher = FMLClientHandler.instance().getClient().getBlockRendererDispatcher();
-		IBakedModel model = dispatcher.getBlockModelShapes().getModelForState(machine.getWorld().getBlockState(machine.getPos().up()));
-		//		IBakedModel model = dispatcher.getBlockModelShapes().getModelForState(machine.getWorld().getBlockState(machine.getPos()).withProperty(BlockMachine.FACING, EnumFacing.NORTH));
+		IBakedModel model = dispatcher.getBlockModelShapes().getModelForState(state.withProperty(BlockMachine.FACING, EnumFacing.NORTH));
 		FMLClientHandler.instance().getClient().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		drawState(gui, model, dispatcher, 4, 23, -90F, 0F, 1F, 0F);
-		drawState(gui, model, dispatcher, 23, 4, 90F, 1F, 0F, 0F);
-		drawState(gui, model, dispatcher, 23, 23, 180F, 0F, 1F, 0F);
-		drawState(gui, model, dispatcher, 23, 42, -90F, 1F, 0F, 0F);
-		drawState(gui, model, dispatcher, 42, 23, 90F, 0F, 1F, 0F);
-		drawState(gui, model, dispatcher, 42, 42);
+		drawState(gui, model, dispatcher, 4, 23, 90F, 0F, 1F, 0F);
+		drawState(gui, model, dispatcher, 23, 4, -90F, 1F, 0F, 0F);
+		drawState(gui, model, dispatcher, 23, 23);
+		drawState(gui, model, dispatcher, 23, 42, 90F, 1F, 0F, 0F);
+		drawState(gui, model, dispatcher, 42, 23, -90F, 0F, 1F, 0F);
+		drawState(gui, model, dispatcher, 42, 42, 180F, 0F, 1F, 0F);
 	}
 
 	public void drawState(MechGui gui, IBakedModel model, BlockRendererDispatcher dispatcher, int x, int y, float rotAngle, float rotX, float rotY, float rotZ) {
 		GlStateManager.pushMatrix();
-		GlStateManager.translate(8 + gui.xFactor + this.x + x, 8 + gui.yFactor + this.y + y, 100);
-		GlStateManager.rotate(180.0F, 1.0F, 0.0F, 0.0F);
+		GlStateManager.enableDepth();
+		GlStateManager.translate(8 + gui.xFactor + this.x + x, 8 + gui.yFactor + this.y + y, 512);
 		if (rotAngle != 0) {
 			GlStateManager.rotate(rotAngle, rotX, rotY, rotZ);
 		}
-		GlStateManager.scale(16, 16, 16);
-		GlStateManager.translate(-0.5F, -0.5F, -0.5F);
-		dispatcher.getBlockModelRenderer().renderModelBrightnessColor(model, 1.0F, 1.0F, 1.0F, 1.0F);
+		GlStateManager.scale(16F, 16F, 16F);
+		GlStateManager.translate(0.5F, 0.5F, 0.5F);
+		GlStateManager.scale(-1, -1, -1);
+		dispatcher.getBlockModelRenderer().renderModelBrightnessColor(model, 1F, 1F, 1F, 1F);
+		GlStateManager.disableDepth();
 		GlStateManager.popMatrix();
 	}
 
