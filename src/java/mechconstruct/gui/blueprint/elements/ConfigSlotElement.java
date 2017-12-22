a6acf74d84a044902404ec99e32a309e3ec65d94
Prospector <thedoctorsoda@gmail.com>
2017-12-22 09:24:34 -0800

    continue progress

--- src/java/mechconstruct/gui/blueprint/elements/ConfigSlotElement.java
+++ src/java/mechconstruct/gui/blueprint/elements/ConfigSlotElement.java
@@ -10,10 +10,14 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.ItemStackHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfigSlotElement extends ElementBase {
 	SlotType type;
 	ItemStackHandler inventory;
 	int id;
+	List<ElementBase> toRemove;
 
 	public ConfigSlotElement(ItemStackHandler slotInventory, int slotId, SlotType type, int x, int y) {
 		super(x, y, type.getButtonSprite());
@@ -21,35 +25,55 @@ public class ConfigSlotElement extends ElementBase {
 		this.inventory = slotInventory;
 		this.id = slotId;
 		addPressAction(((element, gui, provider, mouseX, mouseY) -> {
-			gui.elements.add(new ElementBase(0 - gui.getOffsetFactorX(), 0 - gui.getOffsetFactorY(), Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight) {
+			List<ElementBase> elements = new ArrayList<>();
+			elements.add(new ElementBase(0 - gui.getOffsetFactorX(), 0 - gui.getOffsetFactorY(), Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight) {
 				@Override
 				public void draw(MechGui gui) {
 					super.draw(gui);
 					MechClient.GUI_ASSEMBLER.drawRect(gui, x, y, getWidth(gui.provider), getHeight(gui.provider), 0xA0000000);
 				}
-			});
-			gui.elements.add(new SlotConfigPopupElement(((ConfigSlotElement) element).id, x - 22, y - 22));
-			gui.elements.add(new ElementBase(x + 29, y - 25, Sprite.EXIT_BUTTON).addUpdateAction((g, e) -> {
+			}.addPressAction((element1, gui1, provider1, mouseX1, mouseY1) -> {
+				if (((ConfigSlotElement) element).toRemove != null) {
+					gui.elements.removeAll(((ConfigSlotElement) element).toRemove);
+				}
+			}));
+			elements.add(new SlotConfigPopupElement(((ConfigSlotElement) element).id, x - 22, y - 22));
+			elements.add(new ElementBase(x + 29, y - 25, Sprite.EXIT_BUTTON).addUpdateAction((g, e) -> {
 				if (e.isHovering) {
 					e.container.setSprite(0, Sprite.EXIT_BUTTON_HOVER);
 				} else {
 					e.container.setSprite(0, Sprite.EXIT_BUTTON);
 				}
-			}));
-			gui.elements.add(new ElementBase(x - 18, y - 18, Sprite.AUTO_INPUT_BUTTON).addUpdateAction((g, e) -> {
+			})
+				.addPressAction((element1, gui1, provider1, mouseX1, mouseY1) -> {
+					if (((ConfigSlotElement) element).toRemove != null) {
+						gui.elements.removeAll(((ConfigSlotElement) element).toRemove);
+					}
+				}));
+			elements.add(new ElementBase(x + 42, y + 1, Sprite.AUTO_INPUT_BUTTON).addUpdateAction((g, e) -> {
 				if (e.isHovering) {
 					e.container.setSprite(0, Sprite.AUTO_INPUT_BUTTON_HOVER);
 				} else {
 					e.container.setSprite(0, Sprite.AUTO_INPUT_BUTTON);
 				}
-			}));
-			gui.elements.add(new ElementBase(x - 18, y + 20, Sprite.AUTO_OUTPUT_BUTTON).addUpdateAction((g, e) -> {
+			})
+				.addPressAction((element1, gui1, provider1, mouseX1, mouseY1) -> {
+					//INPUT BUTTON ACTION
+				}));
+			elements.add(new TextElement("Auto Input", 0xFFFFFFFF, x, y + 50));
+			elements.add(new TextElement("Auto Output", 0xFFFFFFFF, x, y + 60));
+			elements.add(new ElementBase(x + 42, y + 20, Sprite.AUTO_OUTPUT_BUTTON).addUpdateAction((g, e) -> {
 				if (e.isHovering) {
 					e.container.setSprite(0, Sprite.AUTO_OUTPUT_BUTTON_HOVER);
 				} else {
 					e.container.setSprite(0, Sprite.AUTO_OUTPUT_BUTTON);
 				}
-			}));
+			})
+				.addPressAction((element1, gui1, provider1, mouseX1, mouseY1) -> {
+					//OUTPUT BUTTON ACTION
+				}));
+			gui.elements.addAll(elements);
+			((ConfigSlotElement) element).toRemove = elements;
 		}));
 	}
 
--- src/java/mechconstruct/gui/blueprint/elements/SlotConfigPopupElement.java
+++ src/java/mechconstruct/gui/blueprint/elements/SlotConfigPopupElement.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -29,33 +30,59 @@ public class SlotConfigPopupElement extends ElementBase {
 		IBlockAccess blockAccess = machine.getWorld();
 		BlockPos pos = machine.getPos();
 		IBlockState state = blockAccess.getBlockState(pos);
+		IBlockState actualState = Blocks.DIRT.getDefaultState().getActualState(blockAccess, pos);
 		BlockRendererDispatcher dispatcher = FMLClientHandler.instance().getClient().getBlockRendererDispatcher();
 		IBakedModel model = dispatcher.getBlockModelShapes().getModelForState(state.withProperty(BlockMachine.FACING, EnumFacing.NORTH));
 		FMLClientHandler.instance().getClient().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		drawState(gui, model, dispatcher, 4, 23, 90F, 0F, 1F, 0F);
-		drawState(gui, model, dispatcher, 23, 4, -90F, 1F, 0F, 0F);
-		drawState(gui, model, dispatcher, 23, 23);
-		drawState(gui, model, dispatcher, 23, 42, 90F, 1F, 0F, 0F);
-		drawState(gui, model, dispatcher, 42, 23, -90F, 0F, 1F, 0F);
-		drawState(gui, model, dispatcher, 42, 42, 180F, 0F, 1F, 0F);
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, 4, 23);
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, 23, -12, -90F, 1F, 0F, 0F);
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, 23, 23, -90F, 0F, 1F, 0F);
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, 23, 42, 90F, 1F, 0F, 0F);
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, 26, 23, 180F, 0F, 1F, 0F);
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, 26, 42, 90F, 0F, 1F, 0F);
 	}
 
-	public void drawState(MechGui gui, IBakedModel model, BlockRendererDispatcher dispatcher, int x, int y, float rotAngle, float rotX, float rotY, float rotZ) {
+	public void drawState(MechGui gui,
+	                      IBlockAccess blockAccess,
+	                      IBakedModel model,
+	                      IBlockState actualState,
+	                      BlockPos pos,
+	                      BlockRendererDispatcher dispatcher,
+	                      int x,
+	                      int y,
+	                      float rotAngle,
+	                      float rotX,
+	                      float rotY,
+	                      float rotZ) {
+
 		GlStateManager.pushMatrix();
 		GlStateManager.enableDepth();
 		GlStateManager.translate(8 + gui.xFactor + this.x + x, 8 + gui.yFactor + this.y + y, 512);
+		GlStateManager.scale(16F, 16F, 16F);
+		GlStateManager.translate(0.5F, 0.5F, 0.5F);
+		GlStateManager.scale(-1, -1, -1);
+		if (rotAngle != 0) {
+			GlStateManager.rotate(rotAngle, rotX, rotY, rotZ);
+		}
+		dispatcher.getBlockModelRenderer().renderModelBrightness(model, actualState, 1F, false);
+		GlStateManager.disableDepth();
+		GlStateManager.popMatrix();
+
+/*		GlStateManager.pushMatrix();
+		GlStateManager.enableDepth();
+		//		GlStateManager.translate(8 + gui.xFactor + this.x + x, 8 + gui.yFactor + this.y + y, 1000);
+		GlStateManager.translate(gui.xFactor + this.x + x, gui.yFactor + this.y + y, 512);
 		if (rotAngle != 0) {
 			GlStateManager.rotate(rotAngle, rotX, rotY, rotZ);
 		}
 		GlStateManager.scale(16F, 16F, 16F);
-		GlStateManager.translate(0.5F, 0.5F, 0.5F);
+		GlStateManager.translate(-0.5F, -0.5F, -0.5F);
 		GlStateManager.scale(-1, -1, -1);
-		dispatcher.getBlockModelRenderer().renderModelBrightnessColor(model, 1F, 1F, 1F, 1F);
 		GlStateManager.disableDepth();
-		GlStateManager.popMatrix();
+		GlStateManager.popMatrix();*/
 	}
 
-	public void drawState(MechGui gui, IBakedModel model, BlockRendererDispatcher dispatcher, int x, int y) {
-		drawState(gui, model, dispatcher, x, y, 0, 0, 0, 0);
+	public void drawState(MechGui gui, IBlockAccess blockAccess, IBakedModel model, IBlockState actualState, BlockPos pos, BlockRendererDispatcher dispatcher, int x, int y) {
+		drawState(gui, blockAccess, model, actualState, pos, dispatcher, x, y, 0, 0, 0, 0);
 	}
 }
